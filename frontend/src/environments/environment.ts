db3c6372d39c111b5bed320140e8e68276995a6b
Houssem DIMASSI <houssemdimassii@gmail.com>
2025-02-06 15:05:59 +0100

    remove env - corection path -

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5000/api',
-    stripePublicKey: 'votre_cle_publique_stripe'
+    stripePublicKey: 'pk_test_51QoRKBIX5ip49IYqaCHyQ8ivhoXwvecPhqMp4F5fAJzs6LBpHfn50OeMdk2AcG1dM0SyXQSZKekyW3SU1q7ix0hX00pzDg9VuL'
   };
